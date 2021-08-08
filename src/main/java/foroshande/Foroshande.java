46ba0df6f05f7ec7111c5598082df889d7855f32
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-08-08 19:01:21 +0200

    static block

--- src/main/java/foroshande/Foroshande.java
+++ src/main/java/foroshande/Foroshande.java
@@ -4,6 +4,13 @@ import khodro.jadid.Mashin;
 
 public class Foroshande {
 
+    public static String[] namFroshandegan = new String[10];
+
+    static {
+        namFroshandegan[0] = "ali";
+        namFroshandegan[1] = "ahmad";
+    }
+
     public void foroshMashin() {
         Mashin.tedadeForosh++;
     }
