4fe45f48a51de001457a83061dcab376277b63ad
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-08-08 19:28:46 +0200

    static class

--- src/main/java/khodro/Ranande.java
+++ src/main/java/khodro/Ranande.java
@@ -1,4 +1,30 @@
 package khodro;
 
+// Outer Class
 public class Ranande {
+
+    private String noeGavahiname = "Paye 1";    // Runtime
+    private static String nam = "Farhad";   // Compile Time
+
+    // Non-static Nested Class : Inner Class
+    public class Gavahiname {
+
+        public void print() {
+            System.out.println("Class Non Static : ");
+            System.out.println(noeGavahiname);
+            System.out.println(nam);
+        }
+
+    }
+
+    // Static Nested Class
+    public static class StaticGavahiname {
+
+        public void print() {
+            System.out.println("Class Static : ");
+            //System.out.println(noeGavahiname);    // Kar nemikonad
+            System.out.println(nam);
+        }
+
+    }
 }
