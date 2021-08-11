d3826ca8f3405a6c1ff5d1a7904886725385fa2d
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-08-12 00:26:20 +0200

    rename test method

--- src/test/java/KashtiTest.java
+++ src/test/java/KashtiTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class KashtiTest {
 
     @Test
-    void protected_method() {
+    void final_method() {
         Khodro kashti = new Kashti();
 
         kashti.ayaDarHaleHarekatAst();
