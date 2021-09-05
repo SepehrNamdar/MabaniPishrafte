f9196fa5cc39ea5555bf2394d0df59b9da98130b
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-09-05 16:19:06 +0200

    try catch

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,8 +4,14 @@ import static java.lang.Double.parseDouble;
 
 public class Calculator {
     public String add(final String adadAval, final String adadDovom) {
-        final double d1 = parseDouble(adadAval);
-        final double d2 = parseDouble(adadDovom);
+        double d1, d2;
+        try {
+            d1 = parseDouble(adadAval);
+            d2 = parseDouble(adadDovom);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Adade vared shode ghbel ghabol nist");
+            throw nfe;
+        }
         final double result = d1 + d2;
         return String.valueOf(result);
     }
