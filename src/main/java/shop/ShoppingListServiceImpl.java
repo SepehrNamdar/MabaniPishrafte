e1dada31c5713ff57d7f5b3c6af76b5d1671565b
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-10-05 12:45:28 +0200

    Separate front & back

---
+++ src/main/java/shop/ShoppingListServiceImpl.java
@@ -0,0 +1,16 @@
+package shop;
+
+import java.util.List;
+
+public class ShoppingListServiceImpl implements ShoppingListService {
+
+    @Override
+    public List<Item> findAllItems() {
+        return null;
+    }
+
+    @Override
+    public void saveItems(List<Item> items) {
+
+    }
+}
