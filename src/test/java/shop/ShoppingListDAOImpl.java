06b4871a34aea86d38dc114558cf0d4a51d90646
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-10-05 23:08:07 +0200

    prepared statement

--- src/test/java/shop/ShoppingListDAOImpl.java
+++ src/test/java/shop/ShoppingListDAOImpl.java
@@ -3,22 +3,59 @@ package shop;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static java.sql.DriverManager.getConnection;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 public class ShoppingListDAOImpl {
 
+    public static final String HOST = "jdbc:mysql://localhost:3306/shop";
+    public static final String USER = "sepehr";
+    public static final String PASS = "7learn";
+    public static final String ERROR = "Connection to database is impossible";
+    public static final String INSERT_SQL = "INSERT INTO item (name, quantity) VALUES (?, ?)";
+    public static final String SELECT_SQL = "SELECT * FROM item ORDER BY id DESC limit 1";
+    public static final String DELETE_SQL = "DELETE FROM item ORDER BY id DESC limit 1";
+
     @Test
     void should_connect_to_MySQL_Database() {
-        try (final Connection con = getConnection("jdbc:mysql://localhost:3306/shop", "sepehr", "7learn")) {
+        try (final Connection con = getConnection(HOST, USER, PASS)) {
             if (con == null) {
-                fail("Connection to database is impossible");
+                fail(ERROR);
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            fail("Connection to database is impossible");
+            fail(ERROR);
+        }
+    }
+
+    @Test
+    void should_insert_read_and_delete_an_item_in_database() {
+        try (final Connection con = getConnection(HOST, USER, PASS);
+             final PreparedStatement insert = con.prepareStatement(INSERT_SQL);
+             final PreparedStatement select = con.prepareStatement(SELECT_SQL);
+             final PreparedStatement delete = con.prepareStatement(DELETE_SQL)) {
+            insert.setString(1, "Orange");
+            insert.setInt(2, 5);
+            insert.executeUpdate();
+
+            final ResultSet resultSet = select.executeQuery();
+            while (resultSet.next()) {
+                final String name = resultSet.getString("name");
+                final int quantity = resultSet.getInt("quantity");
+
+                assertThat(name).isEqualTo("Orange");
+                assertThat(quantity).isEqualTo(5);
+            }
+
+            delete.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail(ERROR);
         }
     }
 }
