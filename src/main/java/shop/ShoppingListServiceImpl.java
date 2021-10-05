1b8848327bd363f70efeb1cebf51bb5902369aa5
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-10-05 16:18:09 +0200

    Backend implementation

--- src/main/java/shop/ShoppingListServiceImpl.java
+++ src/main/java/shop/ShoppingListServiceImpl.java
@@ -6,11 +6,16 @@ public class ShoppingListServiceImpl implements ShoppingListService {
 
     @Override
     public List<Item> findAllItems() {
+        // Get all items from database : Any logic
         return null;
     }
 
     @Override
     public void saveItems(List<Item> items) {
+        for (Item item : items) {
+            item.check();
+        }
 
+        // save all items in database
     }
 }
