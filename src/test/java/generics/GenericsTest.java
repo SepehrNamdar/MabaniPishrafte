48e5555c86bfb6ec535cab095204503085fc5b48
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-11-08 12:50:02 +0100

    Generic syntax

--- src/test/java/generics/GenericsTest.java
+++ src/test/java/generics/GenericsTest.java
@@ -1,10 +1,10 @@
 package generics;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,5 +17,29 @@ public class GenericsTest {
         list.add("10");
 
         assertThat((Integer) list.get(1)).isNotNull();
+    }
+
+    @Test
+    void should_works() {
+        List<Number> list = new ArrayList<>();
+        list.add(5);
+
+        final Integer[] integers = list.toArray(new Integer[0]);
+
+        for (Integer integer : integers) {
+            System.out.println(integer);
+        }
+    }
+
+    @Test
+    void generic_method_syntax() {
+        GenericMethod<Integer> gt = new GenericMethod<>();
+        final Integer[] integers = gt.toArray(new Integer[0], "s");
+    }
+
+    class GenericMethod<E> {
+        <S, T extends E> T[] toArray(T[] a, S s) {
+            return null;
+        }
     }
 }
